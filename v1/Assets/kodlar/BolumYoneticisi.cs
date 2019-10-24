fc3c95fb67f961b268136d9208c69e057d4fee05
Meliha GÜLBAY <mlhgulbay@gmail.com>
2019-10-24 20:24:04 +0300

    v1 baslangic

---
+++ v1/Assets/kodlar/BolumYoneticisi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolumYoneticisi : MonoBehaviour
+{
+   public void BolumAc(string BolumIsmi)
+   {
+       Application.LoadLevel(BolumIsmi);
+   }
+
+
+    public void Cikis()
+    {
+        Application.Quit();
+    }
+}
---
+++ v1/Assets/kodlar/KameraAyarlari.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraAyarlari : MonoBehaviour
+{
+    public bool ArkaPlanRenkAktif = true;
+    public static int[] ArkaPlanRGB = {0,0,0};
+
+    private void Start()
+    {
+        if (ArkaPlanRenkAktif)
+        {
+            gameObject.GetComponent<Camera>().backgroundColor = new Color(ArkaPlanRGB[0], ArkaPlanRGB[1], ArkaPlanRGB[2]);
+        }
+    }
+}
