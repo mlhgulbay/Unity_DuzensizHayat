35cc5af61aacabf7fd72ee2f5f36b951124f2429
Meliha GÜLBAY <mlhgulbay@gmail.com>
2019-10-25 21:18:22 +0300

    v1 Tamamlandı

--- v1/Assets/kodlar/BolumYoneticisi.cs
+++ v1/Assets/kodlar/BolumYoneticisi.cs
@@ -6,12 +6,13 @@ public class BolumYoneticisi : MonoBehaviour
 {
    public void BolumAc(string BolumIsmi)
    {
-       Application.LoadLevel(BolumIsmi);
+       Application.LoadLevel(BolumIsmi);// "BolumIsmi" değişkeninde yazan Sahne ismini yükler. Yani Örneğin "baslangic" sayfasını (sahnesini,schene) açar
    }
 
 
     public void Cikis()
     {
-        Application.Quit();
+        Application.Quit(); // Uygulamayı kapatır. Bu kodun çalışıp uygulamanın kapanabilmesi için derlenmiş (.exe) uygulamasında denemek lazım.
+                            // Unity geliştirici mod olduğu için Application.Quit komutu çalışmayacaktır.
     }
 }
